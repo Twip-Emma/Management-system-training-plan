ac7907c53eef639f94568868f3f311616368177d
七画一只妖 <1157529280@qq.com>
2021-12-27 14:47:56 +0800

    Initial commit

---
+++ src/main/java/top/twip/managementsystemtrainingplan/ManagementSystemTrainingPlanApplication.java
@@ -0,0 +1,11 @@
+package top.twip.managementsystemtrainingplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemTrainingPlanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementSystemTrainingPlanApplication.class, args);
+    }
+}
---
+++ src/test/java/top/twip/managementsystemtrainingplan/ManagementSystemTrainingPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package top.twip.managementsystemtrainingplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemTrainingPlanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
