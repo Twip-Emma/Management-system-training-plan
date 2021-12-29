822c81980eecf2e581c01dea254df22831dd219c
七画一只妖 <1157529280@qq.com>
2021-12-29 17:23:06 +0800

    修改状态码枚举类

--- src/main/java/top/twip/managementsystemtrainingplan/utils/code/Code.java
+++ src/main/java/top/twip/managementsystemtrainingplan/utils/code/Code.java
@@ -1,17 +1,22 @@
 package top.twip.managementsystemtrainingplan.utils.code;
 
-import lombok.Data;
-import org.springframework.stereotype.Component;
-
-import java.util.Map;
-
 /**
  * @Author: 七画一只妖
- * @Date: 2021/12/28 11:30
+ * @Date: 2021/12/29 17:16
  */
-@Data
-public class Code {
-    private final Map<String,String> codes;
-
-
+public enum Code {
+    success{
+        public Integer getCode(){ return 10000; }
+        public String getMessage(){ return "请求成功"; }
+    },
+    fail{
+        public Integer getCode(){ return 40000; }
+        public String getMessage(){
+            return "请求失败";
+        }
+    },
+    successPlus{
+        public Integer getCode(){ return 10001; }
+        public String getMessage(){return "请求成功，但是出现部分异常";}
+    }
 }
